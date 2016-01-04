485e086fe47ebe5acf8ce88f49530cb9fe64853a
Andrew Hobden <andrew@hoverbear.org>
2016-01-04 00:20:09 -0800

    Multirust

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -8,20 +8,20 @@ setup()
 {
     mkdir -p $BUILD_DIR/src
     cat > $BUILD_DIR/Cargo.toml <<EOF
-[package]
-name = "$PROJECT"
-version = "0.1.0"
-authors = ["Andrew Hobden <andrew@hoverbear.org>"]
+    [package]
+    name = "$PROJECT"
+    version = "0.1.0"
+    authors = ["Andrew Hobden <andrew@hoverbear.org>"]
 EOF
     cat > $BUILD_DIR/src/main.rs <<EOF
-fn main() {
-    println!("Hello world!");
-}
+    fn main() {
+        println!("Hello world!");
+    }
 
-#[test]
-fn it_works() {
-    assert(true);
-}
+    #[test]
+    fn it_works() {
+        assert(true);
+    }
 EOF
 }
 
@@ -34,24 +34,11 @@ testDefault()
 {
     setup
 
-    # Check Basic
-    echo "Default: Testing Basic"
     compile
 
-    assertCaptured "------> Installing Rustup."
-    assertCaptured "------> Using channel as nightly (default)."
-    assertCaptured "------> No cached Rust detected."
-    assertCaptured "------> Installing latest Rust nightly."
-    assertCaptured "------> Compiling Application."
-
-    # Check Cache
-    echo "Default: Testing Caching"
-    compile
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as nightly (default)."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Pre-cached up-to-date Rust."
-    assertCaptured "------> Compiling Application."
+    assertCaptured "-----> Fetching multirust."
+    assertCaptured "-----> Setting override to \"nightly\" (default)."
+    assertCaptured "-----> Compiling Application."
 
     cleanup
 }
@@ -60,51 +47,15 @@ testNightly()
 {
     setup
     cat >> $BUILD_DIR/Cargo.toml <<EOF
-[target.heroku]
-channel = "nightly"
+    [target.heroku]
+    override = "nightly"
 EOF
 
-    # Check Basic
-    echo "Nightly: Testing Basic"
-    compile
-    assertCaptured "------> Installing Rustup."
-    assertCaptured "------> Using channel as nightly."
-    assertCaptured "------> No cached Rust detected."
-    assertCaptured "------> Installing latest Rust nightly."
-    assertCaptured "------> Compiling Application."
-
-    # Check Cache
-    echo "Nightly: Testing Caching"
-    compile
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as nightly."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Pre-cached up-to-date Rust."
-    assertCaptured "------> Compiling Application."
-
-    # Check Specific Date
-    echo "Nightly: Testing Specific Date"
-    cat >> $BUILD_DIR/Cargo.toml <<EOF
-date = "2015-12-12"
-EOF
     compile
 
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as nightly."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Pre-cached up-to-date Rust."
-    assertCaptured "------> Compiling Application."
-
-    # Check Updating.
-    echo "Nightly: Testing Updating"
-    sed -i "$ d" $BUILD_DIR/Cargo.toml # Delete the last line.
-    compile
-
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as nightly."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Pre-cached up-to-date Rust."
-    assertCaptured "------> Compiling Application."
+    assertCaptured "-----> Fetching multirust."
+    assertCaptured "-----> Setting override to \"nightly\"."
+    assertCaptured "-----> Compiling Application."
 
     cleanup
 }
@@ -113,55 +64,15 @@ testBeta()
 {
     setup
     cat >> $BUILD_DIR/Cargo.toml <<EOF
-[target.heroku]
-channel = "beta"
+    [target.heroku]
+    override = "beta"
 EOF
 
-    # Check Basic.
-    echo "Beta: Testing Basic"
     compile
-    assertCaptured "------> Installing Rustup."
-    assertCaptured "------> Using channel as beta."
-    assertCaptured "------> No cached Rust detected."
-    assertCaptured "------> Installing latest Rust beta."
-    assertCaptured "------> Compiling Application."
 
-    # Check Cache.
-    echo "Beta: Testing Caching"
-    compile
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as beta."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Pre-cached up-to-date Rust."
-    assertCaptured "------> Compiling Application."
-
-# TODO: This is currently not tested because I'm only able to find one version of the beta.
-#     # Check specific Date
-#     echo "Beta: Testing Specific Date"
-#     cat >> $BUILD_DIR/Cargo.toml <<EOF
-# date = "2015-12-10"
-# EOF
-#     compile
-#
-#     echo "$(cat $STD_OUT)"
-#     echo "$(cat $STD_ERR)"
-#
-#     assertCaptured "------> Pre-cached Rustup."
-#     assertCaptured "------> Using channel as beta."
-#     assertCaptured "------> Cached Rust detected, checking..."
-#     assertCaptured "------> Pre-cached up-to-date Rust."
-#     assertCaptured "------> Compiling Application."
-#
-#     # Check Upgrading
-#     echo "Beta: Testing Upgrading"
-#     sed -i "$ d" $BUILD_DIR/Cargo.toml # Delete the last line.
-#     compile
-#
-#     assertCaptured "------> Pre-cached Rustup."
-#     assertCaptured "------> Using channel as beta."
-#     assertCaptured "------> Cached Rust detected, checking..."
-#     assertCaptured "------> Pre-cached up-to-date Rust."
-#     assertCaptured "------> Compiling Application."
+    assertCaptured "-----> Fetching multirust."
+    assertCaptured "-----> Setting override to \"beta\"."
+    assertCaptured "-----> Compiling Application."
 
     cleanup
 }
@@ -170,51 +81,15 @@ testStable()
 {
     setup
     cat >> $BUILD_DIR/Cargo.toml <<EOF
-[target.heroku]
-channel = "stable"
+    [target.heroku]
+    override = "stable"
 EOF
 
-    # Check Basic
-    echo "Stable: Testing Basic"
-    compile
-    assertCaptured "------> Installing Rustup."
-    assertCaptured "------> Using channel as stable."
-    assertCaptured "------> No cached Rust detected."
-    assertCaptured "------> Installing latest Rust stable."
-    assertCaptured "------> Compiling Application."
-
-    # Check Caching
-    echo "Stable: Testing Caching"
-    compile
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as stable."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Compiling Application."
-
-    # Check Specific Revision
-    echo "Stable: Testing Specific Revision"
-    cat >> $BUILD_DIR/Cargo.toml <<EOF
-revision = "1.4.0"
-EOF
     compile
 
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as stable."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Rust revision not what is specified by \$RUSTC_REVISION."
-    assertCaptured "------> Installing Rust revision 1.4.0."
-    assertCaptured "------> Compiling Application."
-
-    # Check Updating
-    echo "Stable: Testing Updating"
-    sed -i "$ d" $BUILD_DIR/Cargo.toml # Delete the last line.
-    compile
-
-    assertCaptured "------> Pre-cached Rustup."
-    assertCaptured "------> Using channel as stable."
-    assertCaptured "------> Cached Rust detected, checking..."
-    assertCaptured "------> Rust revision not specified, installing newest stable."
-    assertCaptured "------> Compiling Application."
+    assertCaptured "-----> Fetching multirust."
+    assertCaptured "-----> Setting override to \"stable\"."
+    assertCaptured "-----> Compiling Application."
 
     cleanup
 }
